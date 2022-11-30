9417d236cd4e867df01aca8be93ce735e4d0742d
corey.wagehoft <corey.wagehoft@relativity.com>
2022-11-30 09:09:53 -0700

    fixing issue with no annotations
    Signed-off-by: Corey Wagehoft <corey.wagehoft@gmail.com>

--- opensearch-operator/pkg/builders/dashboards_test.go
+++ opensearch-operator/pkg/builders/dashboards_test.go
@@ -5,12 +5,16 @@ import (
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	opsterv1 "opensearch.opster.io/api/v1"
+	"opensearch.opster.io/pkg/helpers"
 )
 
 var _ = Describe("Builders", func() {
 	When("building the dashboards deployment with annotations supplied", func() {
 		It("should populate the dashboard pod spec with annotations", func() {
 			clusterName := "dashboards-add-annotations"
+			reconcilerAnnotation := map[string]string{
+				helpers.DashboardChecksumName: "somerandomstring",
+			}
 			spec := opsterv1.OpenSearchCluster{
 				ObjectMeta: metav1.ObjectMeta{Name: clusterName, Namespace: clusterName, UID: "dummyuid"},
 				Spec: opsterv1.ClusterSpec{
@@ -23,7 +27,7 @@ var _ = Describe("Builders", func() {
 						},
 					},
 				}}
-			var result = NewDashboardsDeploymentForCR(&spec, nil, nil, nil)
+			var result = NewDashboardsDeploymentForCR(&spec, nil, nil, reconcilerAnnotation)
 			Expect(result.Spec.Template.Annotations).To(Equal(map[string]string{
 				"testAnnotationKey":  "testValue",
 				"testAnnotationKey2": "testValue2",
